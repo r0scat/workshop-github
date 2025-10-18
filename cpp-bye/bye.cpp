7403160207df0360c5c652d401524c0e15177068
r0scat <116912444+r0scat@users.noreply.github.com>
2025-10-18 10:38:42 +0200

    Revert "Add python3 bye"

--- cpp-bye/bye.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-
-int main()
-{
-	std::cout << "Bye from Unikraft!" << std::endl;
-	return 0;
-}
--- python3-bye/hello.py
+++
@@ -1,9 +0,0 @@
-import sys
-
-
-def main():
-    print("Bye from Unikraft!")
-
-
-if __name__ == "__main__":
-    sys.exit(main())
